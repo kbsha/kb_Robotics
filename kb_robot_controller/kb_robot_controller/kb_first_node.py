f39db5d6146948df17d7d8bf3ce9f1917a74795a
kb <kibremogesf@gmail.com>
2025-12-01 23:12:02 +0800

    first node complet

---
+++ kb_robot_controller/kb_robot_controller/kb_first_node.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+
+    def __init__(self):  # Fix the init method (double underscores)
+        super().__init__("first_node")  # Fix the call to super()
+        #self.get_logger().info("Hello, this is KB ROS2 new v2 ")  # Log message
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("hello kb"+str(self.counter_))
+        self.counter_+= 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()  # Create the node instance
+    rclpy.spin(node)  # Keep the node running and processing callbacks
+
+    rclpy.shutdown()  # Shutdown the ROS client library
+
+if __name__ == '__main__':  # Corrected if condition
+    main()
---
+++ kb_robot_controller/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'kb_robot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jetson',
+    maintainer_email='jetson@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "kb_test_node = kb_robot_controller.kb_first_node:main"
+        ],
+    },
+)
